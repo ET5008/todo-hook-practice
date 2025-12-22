b7b258e5b408af9c835c0eabbe9785f4a079af83
Ethan Tran <tran.ethan.000@gmail.com>
2025-12-22 05:10:54 -0800

    v0.0.0 Improving code editor navigation for efficiency. Researched APIs to use for practice.

---
+++ client/src/layout/nav.jsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+    return (
+        <nav className="flex flex-col text-center bg-slate-400 p-8 w-screen items-center">
+            <h1 className="flex text-center">API practice</h1>
+        </nav>
+    )
+}

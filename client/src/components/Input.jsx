5f00c442c11eed63d2417865d80b9324b4431420
Ethan Tran <tran.ethan.000@gmail.com>
2025-12-24 05:02:13 -0800

    v0.0.1 Did more learning on input fields

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+
 export default function inputField(){
+    const [input, setInput] = useState('')
+
     return(
         <form action="" className="flex flex-col items-center space-y-1 border p-4">
             <label htmlFor="input">Input field</label>
-            <input type="text" id="input" name="input" className="border rounded" />
-            <input type="button" className="hover:cursor-pointer border rounded mt-2 p-1" value={'Send call'} />
+            <input type="text" id="input" name="input" className="border rounded" value={input} onChange={(e) =>
+                {setInput(e.target.value) 
+                console.log(input)}} />
+            <input type="submit" className="hover:cursor-pointer border rounded mt-2 p-1" value={'Send call'} />
         </form>
     )
 }

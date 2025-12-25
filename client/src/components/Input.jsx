3c4ff1d9410660a080f825d7bb4c044ab3304d87
Ethan Tran <tran.ethan.000@gmail.com>
2025-12-25 03:48:17 -0800

    v0.0.1 Learned more neovim commands for copy and paste. Displayed input text, and prevented page reload when sending call.

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export default function inputField(){
-    const [input, setInput] = useState('')
+export default function inputField() {
+  const [input, setInput] = useState("");
 
-    return(
-        <form action="" className="flex flex-col items-center space-y-1 border p-4">
-            <label htmlFor="input">Input field</label>
-            <input type="text" id="input" name="input" className="border rounded" value={input} onChange={(e) =>
-                {setInput(e.target.value) 
-                console.log(input)}} />
-            <input type="submit" className="hover:cursor-pointer border rounded mt-2 p-1" value={'Send call'} />
-        </form>
-    )
+  return (
+   <>
+    <form action="" className="flex flex-col items-center space-y-1 border p-4"  onSubmit={(e) => {
+        e.preventDefault()
+        console.log(input)
+        
+    
+        }}>
+        <label htmlFor="input">Input field</label>
+        <input
+            type="text"
+            id="input"
+            name="input"
+            className="border rounded"
+            value={input}
+            onChange={(e) => {
+            setInput(e.target.value);
+            console.log(input);
+            }}
+        />
+        <input
+            type="submit"
+            className="hover:cursor-pointer border rounded mt-2 p-1"
+            value={"Send call"}
+            />
+    </form>
+    <div>
+        <p className="text-2xl">{input}</p>
+    </div>
+   </>
+   
+
+  );
 }

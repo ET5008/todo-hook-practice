af21fd5da4a857def02ba999e6a43746217a5b81
Ethan Tran <tran.ethan.000@gmail.com>
2025-12-27 04:07:15 -0800

    v0.0.2 minor changes, busy day

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -2,55 +2,59 @@ import { useState } from "react";
 
 export default function inputField() {
   const [input, setInput] = useState("");
-  const [request, setRequest] = useState("")
+  const [request, setRequest] = useState("Get");
   return (
-   <>
-    <form action="" className="flex flex-col items-center space-y-1 border p-4"  onSubmit={(e) => {
-        e.preventDefault()
-        console.log(input)
-        
-    
-        }}>
+    <>
+      <form
+        action=""
+        className="flex flex-col items-center space-y-1 border p-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(input);
+        }}
+      >
         <div className="space-x-3">
-            <label htmlFor="input">Input field</label>
-            <input
-                type="text"
-                id="input"
-                name="input"
-                className="border rounded"
-                value={input}
-                onChange={(e) => {
-                setInput(e.target.value);
-                console.log(input);
-                }}
-            />
+          <label htmlFor="input">Input field</label>
+          <input
+            type="text"
+            id="input"
+            name="input"
+            className="border rounded"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+              console.log(input);
+            }}
+          />
         </div>
         <div className="space-x-3">
-            <label htmlFor="request-select">Request type</label>
-            <select value = {request} name="request" id="request-select" className="border rounded" onChange={ (e) =>
-              {
-                setRequest(e.target.value)
-                }
-            }>
-                <option value="Get">Get</option>
-                <option value="Post">Post</option>
-                <option value="Put">Put</option>
-                <option value="Delete">Delete</option>
-            </select>
-
+          <label htmlFor="request-select">Request type</label>
+          <select
+            value={request}
+            name="request"
+            id="request-select"
+            className="border rounded"
+            onChange={(e) => {
+              setRequest(e.target.value);
+            }}
+          >
+            <option value="Get">Get</option>
+            <option value="Post">Post</option>
+            <option value="Put">Put</option>
+            <option value="Delete">Delete</option>
+          </select>
         </div>
         <input
-            type="submit"
-            className="hover:cursor-pointer border rounded mt-2 p-1"
-            value={"Send call"}
-            />
-        
-    </form>
-    <div>
-        <p className="text-2xl">{input}</p>
-    </div>
-   </>
-   
-
+          type="submit"
+          className="hover:cursor-pointer border rounded mt-2 p-1"
+          value={"Send call"}
+        />
+      </form>
+      <div>
+        <h1>Request: {request}</h1>
+        <p className="text-xl">API endpoint: {input}</p>
+        <p className="text-2xl">Output: </p>
+      </div>
+    </>
   );
 }
